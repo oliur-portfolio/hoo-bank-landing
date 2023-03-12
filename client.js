af515254eaecb791a27902ddaee22a281f895794
Oliur Rahman <oliurr941@gmail.com>
2023-03-12 18:48:36 +0600

    rules add

--- client.js
+++ client.js
@@ -2,14 +2,14 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "xwv713jc",
-  dataset: "production",
-  apiVersion: "2022-03-25",
-  useCdn: false,
+    projectId: "xwv713jc",
+    dataset: "production",
+    apiVersion: "2022-03-25",
+    useCdn: false,
 });
 
 const builder = imageUrlBuilder(client);
 
 export function urlFor(source) {
-  return builder.image(source);
+    return builder.image(source);
 }
